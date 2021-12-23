ad950ac29368ffa6ce2ae68346373f47ca209f55
Ben Uthoff <81537940+benuthoff@users.noreply.github.com>
2021-12-23 15:54:01 -0500

    Scrivi 0.13

---
+++ base/openfile.js
@@ -0,0 +1,16 @@
+// File Types and Present File Management for Scrivi
+// Ben Uthoff
+
+var filetypes = {
+	'simple': {
+		'metadata': {},
+		'template': {}
+	}
+};
+
+var currentfile;
+var unsavedchanges = false;
+
+function newFile() {
+	
+};
