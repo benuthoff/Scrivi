69b0fac50e0d573fc3ba8680253d5c3cd26ac965
Ben Uthoff <81537940+benuthoff@users.noreply.github.com>
2021-12-22 21:12:16 -0500

    Scrivi 0.9

--- base/filesmenu.js
+++ base/filesmenu.js
@@ -5,3 +5,7 @@ function toggleFilesMenu() {
 	$('#filesmenu_blind').toggleClass('visible');
 	$('body').toggleClass('menu_blur');
 };
+
+function isValidFileName(str) {
+	let rg = /[A-Za-z0-9()!@#^&()_+;.,]+/g;
+};
