ed7e0e87c325cbf8614728c18b6f723d62e01f08
Ben Uthoff <81537940+benuthoff@users.noreply.github.com>
2021-12-23 15:53:12 -0500

    Scrivi 0.12

--- base/database.js
+++ base/database.js
@@ -6,21 +6,12 @@
 request.onerror = ()=>{ createNotif('Error Deleting "ScriviDB".', {icon: 'alert-triangle', color: 'var(--theme-notiferror)'}) };*/
 
 
-/* Test Data */
-var notes = [
-	{'fileName': 'Reminder', 'fileType': 'Simple', 'author': 'Ben Uthoff', 'metadata': {
-		'title': 'Reminder', 'text': '  - get milk\n - get bread'
-	}},
-	{'fileName': 'Ebitto "Ebi" Karatasi', 'fileType': 'CharacterSheet', 'author': 'None', 'metadata': {
-		'name': 'Ebitto "Ebi" Karatasi', 'class': 'Warlock', 'race': 'Tiefling', 'level': 1, 'experience': 100
-	}}	
-];
-
-
 /* For Test Purposes - Create Database */
 var db;
 var request = window.indexedDB.open('ScriviDB', 1);
 request.onupgradeneeded = function(event) {
+
+	createNotif('Creating ScriviDB...', {icon: 'loader'});
 	
 	db = event.target.result;
 
@@ -29,8 +20,9 @@ request.onupgradeneeded = function(event) {
 	nstore.createIndex('fileType', 'fileType', { unique: false });
 	nstore.createIndex('author', 'author', { unique: false })
 	nstore.createIndex('metadata', 'metadata', { unique: false });
-	nstore.add(notes[0]);
-	nstore.add(notes[1]);
+	nstore.createIndex('template', 'template', { unique: false });
+	//nstore.add(notes[0]);
+	//nstore.add(notes[1]);
 
 	// Create User Metadata Storage
 	var ustore = db.createObjectStore('userdata', { keyPath: 'label'});
